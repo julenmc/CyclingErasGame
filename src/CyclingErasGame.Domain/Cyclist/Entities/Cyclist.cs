bbddd816623bb0bb92436b2c8526cc2be6426a09
Julen <jmcarrcas8@gmail.com>
2026-07-22 19:57:46 +0200

    feat(Cyclist): added VO for measures

--- src/CyclingErasGame.Domain/Cyclist/Entities/Cyclist.cs
+++ src/CyclingErasGame.Domain/Cyclist/Entities/Cyclist.cs
@@ -1,12 +1,18 @@
-﻿namespace CyclingErasGame.Domain.Cyclist.Entities;
+﻿using CyclingErasGame.Domain.Cyclist.ValueObjects;
+
+namespace CyclingErasGame.Domain.Cyclist.Entities;
 
 public class Cyclist
 {
     public Guid Id { get; }
 
+    public Measures Measures { get; }
+
     public Cyclist(
-        Guid id) 
+        Guid id,
+        Measures measures) 
     { 
         Id = id;
+        Measures = measures;
     }
 }
