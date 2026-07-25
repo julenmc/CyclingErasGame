53346be7aec009ce9509cf15c7b1338d7deed108
Julen <jmcarrcas8@gmail.com>
2026-07-25 08:59:49 +0200

    feat(Simulation): created randomizator interface

---
+++ src/CyclingErasGame.Domain/Common/Interfaces/IRandomProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace CyclingErasGame.Domain.Common.Interfaces;
+
+public interface IRandomProvider
+{
+    /// <summary>
+    /// Method to check if an action with a given probability has to be made
+    /// </summary>
+    /// <param name="probability">Probability to make the action</param>
+    /// <returns>True when action has to be made. False when not.</returns>
+    bool DoAction(double probability);
+}
