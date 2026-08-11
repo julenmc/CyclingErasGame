6df1be88aa131de8612fae8fd8c783b81d21e1ad
Julen <jmcarrcas8@gmail.com>
2026-08-11 15:43:32 +0200

    feat: added power interval times

---
+++ src/CyclingErasGame.Domain/Common/Constants/CommonConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace CyclingErasGame.Domain.Common.Constants;
+
+internal static class CommonConstants
+{
+    internal const double ShortIntervalSeconds = 10;        // 10 seconds (below this it's 100% short interval power)
+    internal const double MediumIntervalSeconds = 5 * 60;   // 5 minutes (below this it's mixed with short power, above with long power)
+    internal const double LongIntervalSeconds = 20 * 60;    // 20 minutes (above this it's 100% long interval power)
+}
