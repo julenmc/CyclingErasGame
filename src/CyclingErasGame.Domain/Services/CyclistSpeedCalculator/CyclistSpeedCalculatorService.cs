d3db4636b58eeb9fd08175ea432e78d54f593e25
Julen <jmcarrcas8@gmail.com>
2026-08-16 09:46:44 +0200

    feat: added speed calculator domain service

---
+++ src/CyclingErasGame.Domain/Services/CyclistSpeedCalculator/CyclistSpeedCalculatorService.cs
@@ -0,0 +1,25 @@
+﻿using CyclingErasGame.Domain.Common.Interfaces.Physics;
+using CyclingErasGame.Domain.Common.ValueObjects;
+using CyclingErasGame.Domain.Services.CyclistPowerCalculator.CyclistPowerCalculator;
+
+namespace CyclingErasGame.Domain.Services.CyclistSpeedCalculator;
+
+public class CyclistSpeedCalculatorService : ICyclistSpeedCalculatorService
+{
+    private readonly ISpeedProvider _speedProvider;
+    private readonly ICyclistPowerCalculatorService _powerCalculatorService;
+
+    public CyclistSpeedCalculatorService(
+        ISpeedProvider speedProvider,
+        ICyclistPowerCalculatorService cyclistPowerCalculator)
+    {
+        _speedProvider = speedProvider;
+        _powerCalculatorService = cyclistPowerCalculator;
+    }
+
+    public double Calculate(RaceConditionContext context, Cyclist.Entities.Cyclist cyclist, Simulation.Entities.Cyclist simulationInfo)
+    {
+        var power = _powerCalculatorService.Calculate(cyclist, simulationInfo);
+        return _speedProvider.GetSpeed(context, power);
+    }
+}
